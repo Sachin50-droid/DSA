8569fcd114a66b2ff7b307933cce770e0d16f23e
Sachin50-droid <76769442+Sachin50-droid@users.noreply.github.com>
2026-08-10 21:42:30 +0530

    Time: 0 ms (100%), Space: 22.6 MB (81.24%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int i = 0;
+
+        for (int j = i+1; j < nums.size(); j++) {
+            
+
+            // if (nums[i] == nums[j]) {
+            //     // i++;
+            //     j++;
+            // }
+
+            if (nums[i] != nums[j]) {
+                i++;
+               
+                nums[i] = nums[j];
+            }
+        }
+
+        return i+1;
+    }
+};
